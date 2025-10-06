ecd5b63be5f0f837cf3730f403b2e7cb8cf6eff2
Murilo Dias de Oliveira <murilodiasdeoliveira2022@gmail.com>
2025-10-06 18:02:51 -0300

    Tasks adicionadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
-import './App.css'
-import Tasks from './assets/components/Tasks'
-import AddTask from './assets/components/AddTask'
+import React from 'react';
+import './App.css';
+import Tasks from './assets/components/Tasks';
+import AddTask from './assets/components/AddTask';
 
 function App() {
+  const [tasks, setTasks] = React.useState([
+    {
+      id: 1,
+      title: 'Estudar React',
+      description: 'Estudar React todos os dias as 19h',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: 'Estudar Node',
+      description: 'Estudar Node todos os dias as 20h',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: 'Estudar JavaScript',
+      description: 'Estudar JavaScript todos os dias as 21h',
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask  />
+    <div
+      className="w-screen h-screen 
+    bg-slate-500 flex justify-center p-6"
+    >
+      <div className="w-[500px] ">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/Tasks.jsx
+++ src/assets/components/Tasks.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { ChevronRight, Trash2 } from 'lucide-react';
+import React from 'react';
 
-const Tasks = () => {
+const Tasks = (props) => {
   return (
-    <div>Tasks</div>
-  )
-}
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md w-full text-left cursor-pointer ${
+              task.isCompleted && 'line-through'
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
+            <ChevronRight />
+          </button>
+          <button onClick={() => props.onDeleteTaskClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
+            <Trash2 />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default Tasks
+export default Tasks;
